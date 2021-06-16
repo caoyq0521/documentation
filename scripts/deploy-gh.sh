993fe7a74f6e775adf305ae231ff7d6549a8f6ba
曹永强 <caoyongqiang@aihujing.com>
2021-06-16 17:20:30 +0800

    fix

--- scripts/deploy-gh.sh
+++ scripts/deploy-gh.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 
 # 把上面的 <USERNAME> 换成你自己的 Github 用户名，<REPO> 换成仓库名，比如我这里就是：
 git push -f git@github.com:caoyq0521/documentation.git master:gh-pages
-git push -f git@github.com:caoyq0521/documentation.git master:gh-pages
+# git push -f git@github.com:caoyq0521/documentation.git master:gh-pages
 
 cd -
